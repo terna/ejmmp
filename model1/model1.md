3bcd39daca25acce92581c96ce5cc4c211d3a6e0
Pietro Terna <pietro.terna@unito.it>
2022-04-20 12:39:36 +0200

    model1 starting

---
+++ model1/model1.md
@@ -0,0 +1,213 @@
+### Stock-flow *model0*, the real part
+
+- We are considering here uniquely the real part of the economy.
+
+  
+
+- No loans, credits, debts, capital stock, interest rate.
+
+
+
+- Enterpreneurs have unlimited funds that they put freely in their activities.
+
+  
+
+- Saving is abstension from consuming.
+
+
+
+- Investments are uniquely replacement ones, without technical progress.
+
+  
+
+### symbol table
+
+[symbol table](https://www.caam.rice.edu/~heinken/latex/symbols.pdf) in $\LaTeX$ 
+
+$\mathcal{N}$ - number of agents, `agentNum`
+
+$\mathcal{N}^E$ - employer number, a uniformly distributed integer number in range $[\mathcal{E}_{min},\mathcal{E}_{max}]$, `employerNum`
+
+$p$ - price `price`
+
+$u$ - unemployment rate `unemploymentRate`
+
+$dimensional~heterogeneity$ is a $true/false$ chooser, forcing increasing firms to attract more workers. `dimensionalHeterogeneity`
+
+$\pi_{i,t}$ - labor productivity, a uniformly distributed decimal number in range $[\pi_{min},\pi_{max}]$,`productivity`
+
+&Delta;$\pi_{i,t}$ - uniperiodal additive productivity correction in range $[$&Delta;$\pi_{min},$ &Delta;$\pi_{max}]$, `productivityDelta`
+
+$n_{i,t}$ - number of workers in a firm, set just in time
+
+$q_{i,t}$ - production in quantity, `production`
+
+$\Pi_{i,t}$ - profit, `profit`
+
+$W$ - wage `wage`
+
+$R_{i,t}$ - revenues, `revenues`
+
+$s_{i,t}$ - sales in quantity, `sales`
+
+$v_{i,t}$ - in**v**entories (final, initial), `initialInventories`, `finalInventories`
+
+$d_{i}$ - deperibility share of the production, `deperibilityShare`, setting $d_{max}$
+
+$c_{i,t}$ - consumption rate, a uniformly distributed decimal number in range $[c_{min},c_{max}]$,`consumptionRate`
+
+$C_{i,t}$ - consumption of $i$ in $t$
+
+$I_{i,t}$  - investment plan  of $i$ in $t$, a uniformly distributed realization in range $[I_{min},I_{max}]$,`investmentProgram`
+
+the investment and consumption actions are repeated in each cycle, looking around randomly for the sellers; currently `nOfConsumptionActions` $=30$ and `nOfInvestmentActions`$=10$; each consumption buy action is at maximum the 20% of the planned consumptions in that cycle; instead, each investment buy action can reach the whole amount of the investment program of the cycle; each buy action is limited by the residual capabilty of the seller
+
+**magically**, the same good can be a consumption item or an investment one
+
+$T$ - number of cycles `ncycles`
+
+$t$ - cycle `cycle`
+
+### agent structure
+
+The structure of a generic agent: it can represent an employer, a worker, an unemployed person.
+
+When an agent is created, the **initialization process** defines:
+
+- its numerical id, `num`
+
+- its employer status, `employer`, set to $false$
+- the id of its employer, `myEmployer`, set to $0$
+- $R_{i,0}$ - `revenues`, initial value  $0$
+- $s_{i,0}$ - `sales`, initial value  $0$
+- $v^i_{i,0}$ - inventories, `initialInventories`, set to $0$ 
+- $v^f_{i,0}$ - inventories, `finalInventories`, set to $0$ 
+- $d_{i}$ - deperibility share, a uniformly distributed decimal number in range $[0,d_{max}]$,`deperibilityShare`
+- $c_{i,0}$ - consumption rate, set to $0$
+- $I_{i,0}$  - investment plan, set to $0$
+- $\Pi_{i,0}$ - profit, set to 0
+
+
+
+- $\pi_{i,t-1}$ is set after the initialization step, if an agent becomes an employer
+
+
+
+we have also a *wallet*, currently unused
+
+
+
+each agent has the **functions**:
+
+
+
+- **produce** function, used only if is an employer, with:
+
+  $\pi_{i,t}=\pi_{i,t-1}+\Delta\pi_{i,t}$
+
+  $q_{i,t}=n_{i,t} \pi_{i,t}$
+
+  
+
+- **payWages**
+
+  if employer, pays $W$ to each employee in each time $t$
+
+  
+
+- **buyConsumptionGoods** 
+
+  $C_{i,t}=c_{i,t} (W+\Pi_{i,t-1})$ 	using $\Pi_{i,t-1}$ we introduce a lag with a (possible) cyclical effect
+
+  
+
+- **buyInvestmentGoods**
+
+  $I_[i,t]$
+
+  
+
+- **makeBalanceSheet**
+
+  $v^f_{i,t}=v^i_{i,t}+q_{i,t}-s_{i,t}-(q_{i,t}-s_{i,t}) d_{i}$
+
+  $R_{i,t}=p s_{i,t}$
+
+  $\Pi_{i,t}=r_{i,t}-W n_{i,t}-p(v^f_{i,t}-v^i_{i,t})$
+
+
+
+### agent setup
+
+- agent basic creation
+
+  
+
+- creation of employer list
+
+  
+
+- selecting employers
+
+  
+
+- with a given (heterogeneous) productivity
+
+  
+
+- assigning to the employer itself as employee
+
+  
+
+- creation of a temporary workforce list of to-be-employed agent, escluding employers (already self employed)
+
+  
+
+- applying the unmployement rate to exclude agents
+
+  
+
+- assigning workforce (to-be-employed agents) to employers, with a reinforcement mechanism, gradually giving more attraction/hiring capacity to those who grow the most
+
+
+
+### meta actions
+
+
+
+- *produceAll* orders to the employers to produce and to collects the results
+
+  
+
+- *payWagesAll* orders to the employers to pay wages, also to themselves
+
+  
+
+- *buyConsumptionGoodsAll* orders to the agents to buy investment goodsorder to the employers to buy investment goods
+
+  
+
+- *buyInvestmentGoodsAll* orders to the employers to buy investment goodsorder to the employers to buy investment goods
+
+
+
+- *makeBalanceSheetAll* with everyone making accounts
+
+
+
+## <p style="color:red">model machine</p>
+
+- a random seed determining the random number sequences
+
+
+
+- a list of actions (meta ones)
+
+
+
+- an engine excecuting the meta actions
+
+
+
+- display tools (their code is hidden into the file `tools.py`)
+
---
+++ model1/tools.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import commonVar as cmv
+
+def makeHist():
+    print("-------------------------------------------\n"+\
+          "employer number "+ str(len(cmv.employerList))+\
+          "\n-------------------------------------------\n")
+    cmv.employerSizeList=[]
+    list(cmv.employerSizeList.append(cmv.employerList[i].num) \
+                                for i in range(len(cmv.employerList)))
+    #print(cmv.employerSizeList)
+
+    plt.hist(cmv.employerSizeList,20,facecolor='orange')
+    plt.xlabel("# of employees")
+    plt.ylabel("# of employeers")
+    plt.title("Distribution of the employers by number of employees")      
+
+
+def plot1():
+    # https://matplotlib.org/3.5.1/gallery/subplots_axes_and_figures/two_scales.html
+    # https://matplotlib.org/3.5.1/tutorials/colors/colors.html
+    myColor1 = 'tab:orange'
+    myColor2 = 'tab:blue'
+    fig1, ax1a = plt.subplots()
+    ax1a.set_ylim([0, max(cmv.totalProductionSeries)])
+    t=range(1,cmv.ncycles+1)
+    ax1a.plot(t, cmv.totalProductionSeries, label="production", color=myColor1)
+    ax1a.tick_params(axis='y', labelcolor=myColor1)
+    
+    ax2a = ax1a.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2a.set_ylim([0, max(cmv.totalProductionInfraVarSeries)])
+    ax2a.plot(t, cmv.totalProductionInfraVarSeries, label="prodInVar", color=myColor2)
+    ax2a.tick_params(axis='y', labelcolor=myColor2)
+    fig1.legend()
+
+def plot2():
+    # https://matplotlib.org/3.5.1/gallery/subplots_axes_and_figures/two_scales.html
+    # https://matplotlib.org/3.5.1/tutorials/colors/colors.html
+    myColor1 = 'tab:orange'
+    myColor2 = 'tab:blue'
+    fig2, ax1b = plt.subplots()
+    ax1b.set_ylim([min(cmv.totalProfitSeries), max(0,max(cmv.totalProfitSeries))])
+    t=range(1,cmv.ncycles+1)
+    ax1b.plot(t, cmv.totalProfitSeries, label="profit", color=myColor1)
+    ax1b.tick_params(axis='y', labelcolor=myColor1)
+    ax1b.plot([1,cmv.ncycles],[0,0], label="zero line", color=myColor1, linestyle='dashed')
+
+    ax2b = ax1b.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2b.set_ylim([0, max(cmv.totalProfitInfraVarSeries)])
+    ax2b.plot(t, cmv.totalProfitInfraVarSeries, label="profInVar", color=myColor2)
+    ax2b.tick_params(axis='y', labelcolor=myColor2)
+    fig2.legend()
+
+def plot3():
+    # https://matplotlib.org/3.5.1/gallery/subplots_axes_and_figures/two_scales.html
+    # https://matplotlib.org/3.5.1/tutorials/colors/colors.html
+    myColor1 = 'tab:orange'
+    myColor2 = 'tab:blue'
+    fig3, ax1c = plt.subplots()
+    ax1c.set_ylim([min(cmv.totalInitialInventoriesSeries), \
+                   max(0,max(cmv.totalInitialInventoriesSeries))])
+    t=range(1,cmv.ncycles+1)
+    ax1c.plot(t, cmv.totalInitialInventoriesSeries, label="initialInventories", \
+              color=myColor1)
+    ax1c.tick_params(axis='y', labelcolor=myColor1)
+    ax1c.plot([1,cmv.ncycles],[0,0], label="zero line", color=myColor1, linestyle='dashed')
+
+    ax2c = ax1c.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2c.set_ylim([0, max(cmv.totalInitialInventoriesInfraVarSeries)])
+    ax2c.plot(t, cmv.totalInitialInventoriesInfraVarSeries, label="initialInventoriesInVar",\
+              color=myColor2)
+    ax2c.tick_params(axis='y', labelcolor=myColor2)
+    fig3.legend()
+
+def plot4():
+    # https://matplotlib.org/3.5.1/gallery/subplots_axes_and_figures/two_scales.html
+    # https://matplotlib.org/3.5.1/tutorials/colors/colors.html
+    myColor1 = 'tab:orange'
+    myColor2 = 'tab:blue'
+    fig4, ax1d = plt.subplots()
+    ax1d.set_ylim([min(cmv.totalFinalInventoriesSeries), \
+                   max(0,max(cmv.totalFinalInventoriesSeries))])
+    t=range(1,cmv.ncycles+1)
+    ax1d.plot(t, cmv.totalFinalInventoriesSeries, label="finalInventories", \
+              color=myColor1)
+    ax1d.tick_params(axis='y', labelcolor=myColor1)
+    ax1d.plot([1,cmv.ncycles],[0,0], label="zero line", color=myColor1, linestyle='dashed')
+
+    ax2d = ax1d.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2d.set_ylim([0, max(cmv.totalFinalInventoriesInfraVarSeries)])
+    ax2d.plot(t, cmv.totalFinalInventoriesInfraVarSeries, label="finalInventoriesInVar",\
+              color=myColor2)
+    ax2d.tick_params(axis='y', labelcolor=myColor2)
+    fig4.legend()
+
+def plot5():
+    # https://matplotlib.org/3.5.1/gallery/subplots_axes_and_figures/two_scales.html
+    # https://matplotlib.org/3.5.1/tutorials/colors/colors.html
+    myColor1 = 'tab:orange'
+    myColor2 = 'tab:blue'
+    fig5, ax1e = plt.subplots()
+    ax1e.set_ylim([0, max(cmv.totalConsumptionSeries)])
+    t=range(1,cmv.ncycles+1)
+    ax1e.plot(t, cmv.totalConsumptionSeries, label="consumption", color=myColor1)
+    ax1e.tick_params(axis='y', labelcolor=myColor1)
+    
+    ax2e = ax1e.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2e.set_ylim([0, max(cmv.totalConsumptionInfraVarSeries)])
+    ax2e.plot(t, cmv.totalConsumptionInfraVarSeries, label="consInVar", color=myColor2)
+    ax2e.tick_params(axis='y', labelcolor=myColor2)
+    fig5.legend()
+
+def plot6():
+    # https://matplotlib.org/3.5.1/gallery/subplots_axes_and_figures/two_scales.html
+    # https://matplotlib.org/3.5.1/tutorials/colors/colors.html
+    myColor1 = 'tab:orange'
+    myColor2 = 'tab:blue'
+    fig6, ax1f = plt.subplots()
+    ax1f.set_ylim([0, max(cmv.totalInvestmentSeries)])
+    t=range(1,cmv.ncycles+1)
+    ax1f.plot(t, cmv.totalInvestmentSeries, label="investment", color=myColor1)
+    ax1f.tick_params(axis='y', labelcolor=myColor1)
+    
+    ax2f = ax1f.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2f.set_ylim([0, max(cmv.totalInvestmentInfraVarSeries)])
+    ax2f.plot(t, cmv.totalInvestmentInfraVarSeries, label="investInVar", color=myColor2)
+    ax2f.tick_params(axis='y', labelcolor=myColor2)
+    fig6.legend()
+    
+
+def nationalAccounts():
+    print("-------------------------------------------\n"+\
+          '"national" accounts'+\
+          "\n-------------------------------------------\n")
+    print("%s\t%s\t%s\t%s\t\t%s\t%s\t%s" % \
+          ("t","total","lost","initial","consum.","invest.","final"))
+    print("\t%s\t%s\t%s\t\t\t\t%s" % ("produc.","produc.","invent.","invent."))
+    for i in range(cmv.ncycles):
+        print("%3d\t%.1f\t%.1f\t%.1f\t    |\t%.1f\t%.1f\t%.1f" % \
+                                   (i+1,cmv.totalProductionSeries[i],
+                                   -cmv.totalLostProductionSeries[i],\
+                                   cmv.totalInitialInventoriesSeries[i],\
+                                   cmv.totalConsumptionSeries[i],\
+                                   cmv.totalInvestmentSeries[i],\
+                                   cmv.totalFinalInventoriesSeries[i]))
+        
+    print("\n\n\n")
