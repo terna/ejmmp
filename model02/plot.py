6859311ce49e87efe2b015b4a0392add3684c267
Pietro Terna <pietro.terna@unito.it>
2022-06-03 11:36:53 +0200

    20220603

--- model02/plot.py
+++ model02/plot.py
@@ -4,6 +4,7 @@ import commonVar as cmv
 def plot(y1lim,y1values,y11abel,y2lim,y2values,y2label):
     # https://matplotlib.org/3.5.1/gallery/subplots_axes_and_figures/two_scales.html
     # https://matplotlib.org/3.5.1/tutorials/colors/colors.html
+    myColor0 = 'tab:green'
     myColor1 = 'tab:orange'
     myColor2 = 'tab:blue'
     fig, ax1 = plt.subplots()
@@ -11,7 +12,7 @@ def plot(y1lim,y1values,y11abel,y2lim,y2values,y2label):
     t=range(1,cmv.ncycles+1)
     ax1.plot(t, y1values, label=y11abel, color=myColor1)
     ax1.tick_params(axis='y', labelcolor=myColor1)
-    ax1.plot([1,cmv.ncycles],[0,0], label="zero line", color=myColor1, linestyle='dashed')
+    ax1.plot([1,cmv.ncycles],[0,0], label="zero line", color=myColor0, linestyle='dashed')
 
     
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
