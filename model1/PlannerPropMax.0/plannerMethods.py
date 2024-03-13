78e87b2b55b59614a099fd4d1886681193185fa6
eleonorapriori <priori.ele@gmail.com>
2024-03-13 18:31:33 +0100

    ci-siamo-quasi

---
+++ model1/PlannerPropMax.0/plannerMethods.py
@@ -0,0 +1,7 @@
+#incrementAndSubstitutions = 'zero', 'random', 'total', 'proportionally' # to be accorded with the name of the folder in the experiments
+incrementAndSubstitutions = 'proportionally'
+
+#askingMaxInvGoodsProduction = 'min', 'regular', 'max' # to be accorded with the name of the folder in the experiments -> PropMin, PropMax
+# relevant only under the case of incrementAndSubstitutions = 'proportionally' 
+askingInvGoodsProduction = 'max' # 'regular' is the default option
+investmentVariation= 2 # used directly if max and as (1/investmentVariation) if min
