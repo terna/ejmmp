2b801b4977255faac431f77ff0eaafc43d6f011b
Pietro Terna <pietro.terna@unito.it>
2024-03-25 00:27:00 +0100

    Multirank

---
+++ model1/PlannerZeroG.1/plannerMethods.py
@@ -0,0 +1,11 @@
+#incrementAndSubstitutions = 'zero', 'random', 'total', 'proportionally' # to be accorded with the name of the folder in the experiments
+incrementAndSubstitutions = 'zero' #'proportionally'
+
+#askingMaxInvGoodsProduction = 'min', 'regular', 'max' # to be accorded with the name of the folder in the experiments -> PropMin, PropMax
+# relevant only under the case of incrementAndSubstitutions = 'proportionally' 
+askingInvGoodsProduction = 'min' # 'regular' is the default option
+investmentVariation= 2 # used directly if max and as (1/investmentVariation) if min
+
+##################################################
+#order generation
+randomOrderGeneration=True
