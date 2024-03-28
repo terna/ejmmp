d9774bcfa57b0e7fdb2ec5273c602d78c2dc197e
eleonorapriori <priori.ele@gmail.com>
2024-03-28 16:08:54 +0100

    problemi-con-max

--- model1/PlannerTot.0/plannerMethods.py
+++
@@ -1,7 +0,0 @@
-#incrementAndSubstitutions = 'zero', 'random', 'total', 'proportionally' # to be accorded with the name of the folder in the experiments
-incrementAndSubstitutions = 'total'
-
-#askingMaxInvGoodsProduction = 'min', 'regular', 'max' # to be accorded with the name of the folder in the experiments -> PropMin, PropMax
-# relevant only under the case of incrementAndSubstitutions = 'proportionally' 
-askingInvGoodsProduction = 'regular' # 'regular' is the default option
-investmentVariation= 1.2 # used directly if max and as (1/investmentVariation) if min
