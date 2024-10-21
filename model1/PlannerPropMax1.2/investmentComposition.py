832764f222d2d4d24b0270f0d49e30646315df79
terna <pietro.terna@unito.it>
2024-10-21 23:37:05 +0200

    eliminating temp print

---
+++ model1/PlannerPropMax1.2/investmentComposition.py
@@ -0,0 +1,14 @@
+#the order in the matrix of the subvectors defining the investment compositions corresponds to that of the "sectorial class" (being the row number of the firm-features file)
+
+#all these shares are meant to be as VALUES
+
+investmentComposition = [\
+[1,0,0,0],\
+[1,0,0,0],\
+[0.5,0.5,0,0],\
+[0.5,0.5,0,0],\
+[0,0,0.4,0.6],\
+[0,0,0.4,0.6],\
+[0,0,0.3,0.7],\
+[0,0,0.3,0.7],\
+]
