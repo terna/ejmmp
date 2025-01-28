b63374f3356d18b43f34d6ad8701ca572c61ca23
terna <pietro.terna@unito.it>
2025-01-29 00:24:46 +0100

    max e min cases

---
+++ model1/robustness-analysis-model1.3/max/PlannerPropMax19.3/plannerMethods.py
@@ -0,0 +1,18 @@
+#incrementAndSubstitutions = 'zero', 'random', 'total', 'proportionally' # to be accorded with the name of the folder in the experiments
+incrementAndSubstitutions = 'proportionally'
+
+#askingMaxInvGoodsProduction = 'min', 'regular', 'max' # to be accorded with the name of the folder in the experiments -> PropMin, PropMax
+# 'min' or 'max' to be used ONLY under the case of incrementAndSubstitutions = 'proportionally' 
+askingInvGoodsProduction = 'max' # OR 'regular' (basic option) OR 'min' OR 'max'
+
+investmentVariation= 0.8 # positive or negative in (-1,+1) 
+#used as 1+investmentVariation for the cases 'min' (must be negative) or 'max' (must be positive)
+
+#################################################
+#order generation (alternatively)
+noOrderGeneration=False
+randomOrderGeneration=True
+
+#################################################
+#modify duration
+durationCoeff=1
